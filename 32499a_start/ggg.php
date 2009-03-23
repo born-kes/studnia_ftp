f1bd2780a7d2f5112bad26d79f93a5d4e696693b
born-kes <born-kes@o2.pl>
2009-03-23 12:20:41 +0100

    v

--- 32499a_start/ggg.php
+++
@@ -1,9 +0,0 @@
-<?php
-          echo"<select name>";
-  $licz=0; 
-    while($licz<30){
-echo"<option value=\"".$licz."\">".$licz."</option>"; 
-$licz++ ;
-}
-            echo"<\select>";
-?>
--- generator_mapy/html.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head><title>KEStools.raportów - Plemiona</title>
-
-<link rel="stylesheet" type="text/css" href="img/stamm1201718544.css">
-<script src="img/mootools.js" type="text/javascript"></script>
-<script src="img/script.js" type="text/javascript"></script>
-</head>
-<br><body>
